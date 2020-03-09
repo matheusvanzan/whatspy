211c21f5386cf403830925ec422d8108922c51cc
Matheus Vanzan <vanzan2015@gmail.com>
2020-03-09 15:31:17 -0300

    Start split Whatsapp class in methods

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name = 'WhatsPy',  
-    version = '0.0.1',
+    version = '0.0.2',
     scripts = [
         'whatspy.sh'
     ],
--- whatspy/main.py
+++ whatspy/main.py
@@ -7,48 +7,53 @@ from whatspy.chrome import Chrome
 
 class Whatsapp:
     
-    @staticmethod
-    def send(message, to):
-        
-        selectors = {
+    def __init__(self):
+        self.selectors = {
             'qrcode': 'canvas',
             'search_input':  '#side .copyable-text.selectable-text',
-            'search_result': '#side span[title="{}"]'.format(to),
+            'search_result': '#side span[title="{}"]',
             'message_input': '#main footer div.copyable-text.selectable-text',
             'message_send':  '#main footer button span[data-icon="send"]'
         }
         
-        chrome = Chrome()
-        chrome.get('http://web.whatsapp.com')
+        self.chrome = Chrome()
+        self.chrome.get('http://web.whatsapp.com')
+        
+        
+    def _check_valid_qrcode(self):
+        # Not logged in
+        small_timeout = 5
+        while not self.chrome.element_exists_at(self.selectors['search_input'], timeout=small_timeout):
+            qrcode = self.chrome.wait_for(self.selectors['qrcode'], timeout=small_timeout)
+            self.chrome.screenshot('qrcode.png')
+            
+            print('Look for whatsapp QRCode inside your running directory.')
+            sleep(small_timeout)
+        
+        print('Whatsapp successfully logged in...')
+        self.chrome.screenshot('screens/1.png')
+
+    def _search_for_chat(self, to):
+        self.chrome.wait_for(self.selectors['search_input']).send_keys(to)
+        self.chrome.wait_for(self.selectors['search_result'].format(to)).click()
+        self.chrome.screenshot('screens/2.png')
+    
+    def _type_message(self, message):
+        self.chrome.wait_for(self.selectors['message_input']).send_keys(message + '\n')
+        # self.chrome.wait_for(selectors['message_send']).click()  # replaced by '\n' on previous line
+        self.chrome.screenshot('screens/3.png')
+    
+    def send(self, message, to):
         
         try:
-        
-            # Not logged in
-            small_timeout = 5
-            while not chrome.element_exists_at(selectors['search_input'], timeout=small_timeout):
-                qrcode = chrome.wait_for(selectors['qrcode'], timeout=small_timeout)
-                chrome.screenshot('qrcode.png')
-                
-                print('Look for whatsapp QRCode inside your running directory.')
-                sleep(small_timeout)
+            self._check_valid_qrcode()
+            self._search_for_chat(to)
+            self._type_message(message)
             
-            print('Whatsapp successfully logged in...')
-            
-            chrome.screenshot('screens/1.png')
-            
-            chrome.wait_for(selectors['search_input']).send_keys(to)
-            chrome.wait_for(selectors['search_result']).click()
-            
-            chrome.screenshot('screens/2.png')
-            
-            chrome.wait_for(selectors['message_input']).send_keys(message + '\n')
-            # chrome.wait_for(selectors['message_send']).click()  # replaced by '\n' on previous line
-            
-            chrome.screenshot('screens/3.png')
         except Exception as e:
             print('An unexpected error occured. Quiting chrome now')
-            chrome.screenshot('screens/error.png')
-            chrome.quit()
+            self.chrome.screenshot('screens/error.png')
+            self.chrome.quit()
             
             raise e
 
@@ -56,5 +61,6 @@ if __name__ == '__main__':
     message = 'whatsapp-bot ' + str(datetime.now())
     to = 'Saldo Contas'
     
-    Whatsapp.send(message, to)
+    whats = Whatsapp()
+    whats.send(message, to)
     
